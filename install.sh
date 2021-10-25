299f3e9811bc81c21d934be4d81831fffe560519
kieljanszen <kiel.legaspi.jose@gmail.com>
2021-10-25 23:35:00 +0800

    rename entrypoint, automate installation

--- install.sh
+++ install.sh
@@ -1,6 +1,5 @@
-# Install Symfony
 composer create-project symfony/skeleton
-
-mv ./skeleton/* ..
-
-rm -rf ./skeleton
+mv skeleton/* .
+mv skeleton/.env skeleton/.gitignore .
+rm -rf skeleton
+docker compose up -d
